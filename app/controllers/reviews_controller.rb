ec7312db62ac37503dcf98c33bc1711b388db483
DoroYoshi <yoshimatsu44dy@gmail.com>
2019-08-24 14:22:59 +0900

    削除機能まで。 次はscopeからだが、 一度サーバー繋いで色々いじってみて確認作業をしたいと思う。

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
-  before_action :set_book, only: [:new, :create]
-  
+  before_action :set_book, only: [:show, :new, :create, :edit]
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+
   def new
     @review = Review.new
   end 
@@ -16,7 +17,26 @@ class ReviewsController < ApplicationController
     else
       render :new
     end
-  end 
+  end
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to @review.book, notice: "レビューを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to @review.book, notice: "レビューを削除しました。"
+  end
   
   private 
   
@@ -26,5 +46,9 @@ class ReviewsController < ApplicationController
   
   def set_book
     @book = Book.find(params[:book_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,17 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :category
   has_many :reviews, dependent: :destroy
+  has_many :users, through: :reviews
   has_one_attached :image
   attribute :new_image
 
   validates :title, presence: true, length: { maximum: 50 }
-  # validates :price, presence: true,
-    # numericality: { 
-      # only_integer: true,
-      # greater_than_or_equal_to: 1
-    # }
-  # validates :publish_date, presence: true
-  # validates :description, presence: true, length: { maximum: 1000 }
+  validates :reading_date, presence: true
+  validates :description, presence: true, length: { maximum: 140 }
 
   before_save do
     self.image = new_image if new_image
